a365b366ca7e777994007521216ce0c07b46c8ce
sol0404 <sol01024134256@gmail.com>
2026-04-28 21:41:49 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260428/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+    int sum1=0, sum2=0;
+    for(int i=0; i<10; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<10; i+=2){
+        sum1+=arr[i];
+    }
+    for(int i=1; i<10; i+=2){
+        sum2+=arr[i];
+    }
+    if(sum1>sum2)
+    cout<<sum1-sum2;
+    else
+    cout<<sum2-sum1;
+    return 0;
+}
