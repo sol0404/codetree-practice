915739e57775630052620b0f91700eb6a319e83b
sol0404 <sol01024134256@gmail.com>
2026-04-29 19:05:57 +0900

    Title: 일의 자리 배열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+    cin>>arr[0]>>arr[1];
+
+    for(int i=2; i<10; i++){
+        arr[i]=(arr[i-1]+arr[i-2])%10;
+    }
+    for(int i=0; i<10; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
